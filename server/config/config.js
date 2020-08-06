705fac99aed2a9615bcf937378cad9193317ef8a
Steve Couch <SGCouch1@gmail.com>
2020-08-05 17:05:09 -0700

    changes startup script

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development'; //used by Heroku
 
 console.log('env **************', env);
 
-if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'test' || env === 'local') {
     var config = require('./config.json');
     var envConfig = config[env];
 
